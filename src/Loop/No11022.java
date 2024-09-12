aff85bef37d7dc82d733b43cb9670823800e5250
sinwoongkang1 <kkace22@gmail.com>
2024-09-13 00:17:35 +0900

    2 problem solved

---
+++ src/Loop/No11022.java
@@ -0,0 +1,20 @@
+package Loop;
+
+import java.io.*;
+public class No11022 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int a = Integer.parseInt(br.readLine());
+        for (int i = 1; i <= a; i++) {
+            String[] input = br.readLine().split(" ");
+            int b = Integer.parseInt(input[0]);
+            int c = Integer.parseInt(input[1]);
+
+            bw.write("Case #"+i+": "+b+" + "+c+ " = "+(b+c) + "\n");
+            bw.flush();
+        }
+        bw.close();
+        br.close();
+    }
+}
